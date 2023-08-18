3da00f0b9566ea18e44ceaec2d9aebcc6d5a69d1
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-08-18 04:02:25 -0300

    Setup `back-end` and inicial setup `front-end`

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,18 @@
-import fastify from "fastify"
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
+const prisma = new PrismaClient()
 
 app.get('/users', () => {
-  return 'Hello World'
+  const users = prisma.user.findMany()
+  return users
 })
 
-app.listen({
-  port: 3333,
-}).then(()=> {
-  console.log('HTTP server running on http://localhost:3333/')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333/')
+  })
