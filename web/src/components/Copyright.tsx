540942de7701cd6eef76f2cef9583c06f81b2696
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-08-21 18:06:05 -0300

    Config github OAuth `front-end`

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito por{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/ogustavohp"
+        className="underline hover:text-gray-100"
+      >
+        @ogustavohp
+      </a>
+      <span></span>
+    </div>
+  )
+}
---
+++ web/src/components/Stripes.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Stripes() {
+  return <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+}
