21a3edb39938f3cd79cb88a71143c9b1124023d5
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-08-21 23:00:54 -0300

    exibir o perfil do usuario `front-end`

---
+++ web/src/app/api/auth/callback/route.ts
@@ -0,0 +1,27 @@
+import { api } from '@/lib/api'
+import { NextRequest, NextResponse } from 'next/server'
+
+const oneSecInSec = 1
+const oneMinuteInSec = oneSecInSec * 60
+const oneHourInSec = oneMinuteInSec * 60
+const oneDayInSec = oneHourInSec * 24
+const oneMonthInSec = oneDayInSec * 30
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url)
+  const code = searchParams.get('code')
+
+  const registerReponse = await api.post('/register', {
+    code,
+  })
+
+  const { token } = registerReponse.data
+
+  const redirectURL = new URL('/', request.url)
+
+  return NextResponse.redirect(redirectURL, {
+    headers: {
+      'Set-Cookie': `token=${token}; Path=/; max-age=${oneMonthInSec};`,
+    },
+  })
+}
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,18 +1,22 @@
+import { cookies } from 'next/headers'
 import BgBlur from '@/components/BgBlur'
 import Copyright from '@/components/Copyright'
 import EmptyMemories from '@/components/EmptyMemories'
 import Hero from '@/components/Hero'
 import Signin from '@/components/Signin'
 import Stripes from '@/components/Stripes'
+import Profile from '@/components/Profile'
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
   return (
     <main className="grid min-h-screen grid-cols-2 ">
       {/* Left */}
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover p-16 px-28 py-16">
         <BgBlur />
         <Stripes />
-        <Signin />
+
+        {isAuthenticated ? <Profile /> : <Signin />}
         <Hero />
         <Copyright />
       </div>
