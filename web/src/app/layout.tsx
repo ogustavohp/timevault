e27e10c31115efd3fedc72f88a380db4424878e3
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-08-22 21:38:06 -0300

    criei o `middleware` de autenticação

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -5,6 +5,13 @@ import {
   Bai_Jamjuree as BaiJamjuree,
 } from 'next/font/google'
 import { ReactNode } from 'react'
+import Copyright from '@/components/Copyright'
+import Hero from '@/components/Hero'
+import Profile from '@/components/Profile'
+import Signin from '@/components/Signin'
+import Stripes from '@/components/Stripes'
+import BgBlur from '@/components/BgBlur'
+import { cookies } from 'next/headers'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 const baiJamjuree = BaiJamjuree({
@@ -20,12 +27,28 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().has('token')
   return (
     <html lang="en">
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2 ">
+          {/* Left */}
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover p-16 px-28 py-16">
+            <BgBlur />
+            <Stripes />
+
+            {isAuthenticated ? <Profile /> : <Signin />}
+            <Hero />
+            <Copyright />
+          </div>
+
+          {/* Right */}
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
